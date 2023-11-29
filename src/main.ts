82571c135faf411784de1215447c445b1ccfecf1
Roger Silva <silvara99@outlook.com>
2023-11-29 11:29:03 -0500

    PWA Elements

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import { setupConfig } from '@ionic/core';
+
+defineCustomElements(window);
 
 // Modo "md" para visualizar en el navegador en la interfaz de Android (Material Design), "ios" para interfaz de iOS
 setupConfig({
